6578ccc84dd1d830cd047ddc43a3fdd8b1e41966
Jan Staschulat (CR/AEX3) <jan.staschulat@de.bosch.com>
2019-05-28 10:52:14 +0000

    refactoring: fixing indentation

--- examples/kobuki/thin_kobuki_node.cxx
+++ examples/kobuki/thin_kobuki_node.cxx
@@ -188,184 +188,184 @@ try {
         PRINT_RCL_ERROR(create_node, rcl_node_init);
     } else {
 
-			//create publisher
-			const char* pose_topic = "robot_pose";
+        //create publisher
+        const char* pose_topic = "robot_pose";
 
-			rcl_publisher_t pub_odom        = rcl_get_zero_initialized_publisher();
-			const rosidl_message_type_support_t * pub_type_support = ROSIDL_GET_MSG_TYPE_SUPPORT(geometry_msgs, msg, Vector3);
-			rcl_publisher_options_t pub_opt = rcl_publisher_get_default_options();
+        rcl_publisher_t pub_odom        = rcl_get_zero_initialized_publisher();
+        const rosidl_message_type_support_t * pub_type_support = ROSIDL_GET_MSG_TYPE_SUPPORT(geometry_msgs, msg, Vector3);
+        rcl_publisher_options_t pub_opt = rcl_publisher_get_default_options();
 
-			rc = rcl_publisher_init(
-				&pub_odom,
-				&node,
-				pub_type_support,
-				pose_topic,
-				&pub_opt);
+        rc = rcl_publisher_init(
+            &pub_odom,
+            &node,
+            pub_type_support,
+            pose_topic,
+            &pub_opt);
 
-			if (rc != RCL_RET_OK) {
-				PRINT_RCL_ERROR(create_publisher, rcl_publisher_init);
-				printf("Failed to create publisher: %s.\n", pose_topic);
-				return -1;
-			} else {
-				printf("Created publisher: %s\n", pose_topic);
-			}
-  
-			//create subscription
-			const char * cmd_vel_topic_name = "cmd_vel";
-			rcl_subscription_t sub_cmd_vel = rcl_get_zero_initialized_subscription();
-			rcl_subscription_options_t subscription_ops = rcl_subscription_get_default_options();
-			const rosidl_message_type_support_t * sub_type_support = ROSIDL_GET_MSG_TYPE_SUPPORT(geometry_msgs, msg, Twist);
-			
-			rc = rcl_subscription_init(
-				&sub_cmd_vel,
-				&node,
-				sub_type_support,
-				cmd_vel_topic_name,
-				&subscription_ops);
+        if (rc != RCL_RET_OK) {
+            PRINT_RCL_ERROR(create_publisher, rcl_publisher_init);
+            printf("Failed to create publisher: %s.\n", pose_topic);
+            return -1;
+        } else {
+            printf("Created publisher: %s\n", pose_topic);
+        }
 
-			if (rc != RCL_RET_OK) {
-				PRINT_RCL_ERROR(create_subscription, rcl_subscription_init);
-				printf("Failed to create subscriber: cmd_vel.\n");
-				return -1;
-			} else {
-				printf("Created subscriber %s:\n",cmd_vel_topic_name);
-			}
+        //create subscription
+        const char * cmd_vel_topic_name = "cmd_vel";
+        rcl_subscription_t sub_cmd_vel = rcl_get_zero_initialized_subscription();
+        rcl_subscription_options_t subscription_ops = rcl_subscription_get_default_options();
+        const rosidl_message_type_support_t * sub_type_support = ROSIDL_GET_MSG_TYPE_SUPPORT(geometry_msgs, msg, Twist);
+        
+        rc = rcl_subscription_init(
+            &sub_cmd_vel,
+            &node,
+            sub_type_support,
+            cmd_vel_topic_name,
+            &subscription_ops);
 
-			// connections are properly setup
-			//TODO replace std::string with char var[256]
-			std::string serial_device;
-			std::string command_vel_topic;
+        if (rc != RCL_RET_OK) {
+            PRINT_RCL_ERROR(create_subscription, rcl_subscription_init);
+            printf("Failed to create subscriber: cmd_vel.\n");
+            return -1;
+        } else {
+            printf("Created subscriber %s:\n",cmd_vel_topic_name);
+        }
 
-			
-			serial_device     = "/dev/ttyS1";
-			command_vel_topic = "cmd_vel";
+        // connections are properly setup
+        //TODO replace std::string with char var[256]
+        std::string serial_device;
+        std::string command_vel_topic;
 
-			robot.connect(serial_device);
+        
+        serial_device     = "/dev/ttyS1";
+        command_vel_topic = "cmd_vel";
 
-			//ros::Rate r(100);
-			//TODO set rate
-			
-			geometry_msgs__msg__Vector3 pose;
-			geometry_msgs__msg__Vector3__init(&pose);
+        robot.connect(serial_device);
 
-			int packet_count = 0;
-			float64 delta    = 0.0;
-			int count = 0;
+        //ros::Rate r(100);
+        //TODO set rate
+        
+        geometry_msgs__msg__Vector3 pose;
+        geometry_msgs__msg__Vector3__init(&pose);
 
-			display_mallinfo();
+        int packet_count = 0;
+        float64 delta    = 0.0;
+        int count = 0;
 
-			while( true ){ // ros::ok() did not work on Olimex with micro-ROS
-				count++;
-				uros_time_t timestamp;
-				robot.receiveData(timestamp);
+        display_mallinfo();
 
-				if (packet_count < robot.packetCount()) {
-						packet_count = robot.packetCount();
+        while( true ){ // ros::ok() did not work on Olimex with micro-ROS
+            count++;
+            uros_time_t timestamp;
+            robot.receiveData(timestamp);
 
-						// send the odometry
-						float x,y,theta, vx, vtheta;
-						robot.getOdometry(x,y,theta,vx,vtheta);
-						/*if((count % 10) == 0) {
-								printf("x=%f, y=%f, theta=%f, vx=%f, vtheta=%f\n",(double) x, (double)y, (double)theta, (double)vx, vtheta);
-						}*/
-						pose.x = x;
-						pose.y = y;
-						pose.z = theta; // HACK!
-						
-						if((count % 5) == 0) {
+            if (packet_count < robot.packetCount()) {
+                    packet_count = robot.packetCount();
 
-								if (! &pub_odom ){
-									fprintf(stderr, "[rcl_publish] null pointer to publisher");
-									return RCL_RET_INVALID_ARGUMENT;
-								}	
-								rcl_publish( &pub_odom, (const void *) &pose);
-						}
-			
-						// imu data
-						//float heading;
-						//robot.getImu(heading, vtheta);
-						//imu.header.seq = seq; //header.seq does not exist
-						//imu.header.stamp = timestamp;
-						//imu.orientation = tf::createQuaternionMsgFromRollPitchYaw(0.0, 0.0, heading);
-						//imu.angular_velocity.z = vtheta;
-						
-						//rclc_publish( pub_imu, (const void *) &imu); 
-						//printf("Sending imu\n");
-						// debug - sending a twist message - because odom and imu data produce error messages in nsh shell as well as in ros2-environment
-						//rclc_publish ( pub_twist, (const void *) & msg_twist);
-						//printf("Sending kobuki_twist\n");
-					}
+                    // send the odometry
+                    float x,y,theta, vx, vtheta;
+                    robot.getOdometry(x,y,theta,vx,vtheta);
+                    /*if((count % 10) == 0) {
+                            printf("x=%f, y=%f, theta=%f, vx=%f, vtheta=%f\n",(double) x, (double)y, (double)theta, (double)vx, vtheta);
+                    }*/
+                    pose.x = x;
+                    pose.y = y;
+                    pose.z = theta; // HACK!
+                    
+                    if((count % 5) == 0) {
 
-				//spin once
-				//rclc_spin_node_once(node, 10);
-				// use do{}while(0) to have port error handling 
-				// by replacing 'return' in error detection parts rclc_spin_node_once  by a 'break')
-				
-				do {
-					uint32_t timeout_ms = 10;
+                            if (! &pub_odom ){
+                                fprintf(stderr, "[rcl_publish] null pointer to publisher");
+                                return RCL_RET_INVALID_ARGUMENT;
+                            }	
+                            rcl_publish( &pub_odom, (const void *) &pose);
+                    }
+        
+                    // imu data
+                    //float heading;
+                    //robot.getImu(heading, vtheta);
+                    //imu.header.seq = seq; //header.seq does not exist
+                    //imu.header.stamp = timestamp;
+                    //imu.orientation = tf::createQuaternionMsgFromRollPitchYaw(0.0, 0.0, heading);
+                    //imu.angular_velocity.z = vtheta;
+                    
+                    //rclc_publish( pub_imu, (const void *) &imu); 
+                    //printf("Sending imu\n");
+                    // debug - sending a twist message - because odom and imu data produce error messages in nsh shell as well as in ros2-environment
+                    //rclc_publish ( pub_twist, (const void *) & msg_twist);
+                    //printf("Sending kobuki_twist\n");
+                }
 
-					// get empty wait set
-					rcl_wait_set_t wait_set = rcl_get_zero_initialized_wait_set();
-					rc = rcl_wait_set_init(&wait_set, 1, 0, 0, 0, 0, rcl_get_default_allocator());
-					if (rc != RCL_RET_OK) {
-						PRINT_RCL_ERROR(spin_node_once, rcl_wait_set_init);
-						break;
-					}
+            //spin once
+            //rclc_spin_node_once(node, 10);
+            // use do{}while(0) to have port error handling 
+            // by replacing 'return' in error detection parts rclc_spin_node_once  by a 'break')
+            
+            do {
+                uint32_t timeout_ms = 10;
 
-					// set rmw fields to NULL
-					rc = rcl_wait_set_clear(&wait_set);
-					if (rc != RCL_RET_OK) {
-						PRINT_RCL_ERROR(spin_node_once, rcl_wait_set_clear_subscriptions);
-						_spin_node_exit(&wait_set);
-						break;
-					}
+                // get empty wait set
+                rcl_wait_set_t wait_set = rcl_get_zero_initialized_wait_set();
+                rc = rcl_wait_set_init(&wait_set, 1, 0, 0, 0, 0, rcl_get_default_allocator());
+                if (rc != RCL_RET_OK) {
+                    PRINT_RCL_ERROR(spin_node_once, rcl_wait_set_init);
+                    break;
+                }
 
-				  size_t index = 0; // is never used - denotes the index of the subscription in the storage container
+                // set rmw fields to NULL
+                rc = rcl_wait_set_clear(&wait_set);
+                if (rc != RCL_RET_OK) {
+                    PRINT_RCL_ERROR(spin_node_once, rcl_wait_set_clear_subscriptions);
+                    _spin_node_exit(&wait_set);
+                    break;
+                }
 
- 					rc = rcl_wait_set_add_subscription(&wait_set, &sub_cmd_vel, &index);
-    			if (rc != RCL_RET_OK) {
-      			PRINT_RCL_ERROR(spin_node_once, rcl_wait_set_add_subscription);
-      			_spin_node_exit(&wait_set);
-      			break;
-					}
+                size_t index = 0; // is never used - denotes the index of the subscription in the storage container
 
-					rc = rcl_wait(&wait_set, RCL_MS_TO_NS(timeout_ms));
-					if (rc == RCL_RET_TIMEOUT) {
-						_spin_node_exit(&wait_set);
-						break;
-					}					
+                rc = rcl_wait_set_add_subscription(&wait_set, &sub_cmd_vel, &index);
+            if (rc != RCL_RET_OK) {
+            PRINT_RCL_ERROR(spin_node_once, rcl_wait_set_add_subscription);
+            _spin_node_exit(&wait_set);
+            break;
+                }
 
-					if (rc != RCL_RET_OK) {
-    				PRINT_RCL_ERROR(spin_node_once, rcl_wait);
-    				_spin_node_exit(&wait_set);
-    				break;
-  				}
+                rc = rcl_wait(&wait_set, RCL_MS_TO_NS(timeout_ms));
+                if (rc == RCL_RET_TIMEOUT) {
+                    _spin_node_exit(&wait_set);
+                    break;
+                }					
 
-					// if RET_OK => one subscription is received because only ONE subscription has been added
-					// just double check here.
-					if ( wait_set.subscriptions[0] ){
-						geometry_msgs__msg__Twist msg;
-						rmw_message_info_t        messageInfo;
-						rc = rcl_take(&sub_cmd_vel, &msg, &messageInfo);
+                if (rc != RCL_RET_OK) {
+                PRINT_RCL_ERROR(spin_node_once, rcl_wait);
+                _spin_node_exit(&wait_set);
+                break;
+            }
 
-						if (rc != RCL_RET_OK) {
-							PRINT_RCL_ERROR(spin_node_once, rcl_take);
-							_spin_node_exit(&wait_set);
-							break;
-						}
-						//rcl_subscription_fini(&sub_cmd_vel, &node);
+                // if RET_OK => one subscription is received because only ONE subscription has been added
+                // just double check here.
+                if ( wait_set.subscriptions[0] ){
+                    geometry_msgs__msg__Twist msg;
+                    rmw_message_info_t        messageInfo;
+                    rc = rcl_take(&sub_cmd_vel, &msg, &messageInfo);
 
-						// call message callback
-						commandVelCallback( &msg );
-						
-					} else {
-						//sanity check
-						fprintf(stderr, "[spin_node_once] no subscription received.\n");
-					}
-          rcl_wait_set_fini(&wait_set);
-				} while (0);
-			}        
-		}
+                    if (rc != RCL_RET_OK) {
+                        PRINT_RCL_ERROR(spin_node_once, rcl_take);
+                        _spin_node_exit(&wait_set);
+                        break;
+                    }
+                    //rcl_subscription_fini(&sub_cmd_vel, &node);
+
+                    // call message callback
+                    commandVelCallback( &msg );
+                    
+                } else {
+                    //sanity check
+                    fprintf(stderr, "[spin_node_once] no subscription received.\n");
+                }
+        rcl_wait_set_fini(&wait_set);
+            } while (0);
+        }        
+    }
 
 		rcl_node_fini( &node);
 
